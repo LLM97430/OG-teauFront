2a51860c23a101f75ea5f2bb211ab4addeffd1d4
LLM97430 <130650980+LLM97430@users.noreply.github.com>
2026-08-08 15:44:09 +0200

    Add site header

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
    new Route("/", "Accueil", "/pages/home.html"),
    new Route("/Galerie", "Galerie", "/pages/galerie.html"),
+   new Route("/Galerie", "Galerie", "/pages/carte.html"),
+   new Route("/Galerie", "Galerie", "/pages/reservations.html"),
+   new Route("/Galerie", "Galerie", "/pages/account.html"),
+   new Route("/Galerie", "Galerie", "/pages/login.html"),
 ];
    //Le titre s'affiche comme ceci : Route.titre - websitename
    export const websiteName = "Ô Gâteau";
