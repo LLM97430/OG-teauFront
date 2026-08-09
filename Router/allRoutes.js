d800c6e6e572b7515e1be663f6ccaf206a156456
LLM97430 <130650980+LLM97430@users.noreply.github.com>
2026-08-09 16:29:00 +0200

    Ajout de la page galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,12 +2,12 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-   new Route("/", "Accueil", "/pages/home.html"),
-   new Route("/Galerie", "Galerie", "/pages/galerie.html"),
-   new Route("/Galerie", "Galerie", "/pages/carte.html"),
-   new Route("/Galerie", "Galerie", "/pages/reservations.html"),
-   new Route("/Galerie", "Galerie", "/pages/account.html"),
-   new Route("/Galerie", "Galerie", "/pages/login.html"),
+   new Route("/", "Accueil", "pages/home.html"),
+   new Route("/galerie", "Galerie", "/pages/galerie.html"),
+   new Route("/carte", "Galerie", "/pages/carte.html"),
+   new Route("/reservations", "Galerie", "/pages/reservations.html"),
+   new Route("/account", "Galerie", "/pages/account.html"),
+   new Route("/login", "Galerie", "/pages/login.html"),
 ];
    //Le titre s'affiche comme ceci : Route.titre - websitename
    export const websiteName = "Ô Gâteau";
