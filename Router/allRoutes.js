625e3c3b1f5ac6f88209281f60f75a4d220a6d99
LLM97430 <130650980+LLM97430@users.noreply.github.com>
2026-08-22 23:11:09 +0200

    Ajout modal édition/suppression galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,10 +4,13 @@ import Route from "./Route.js";
 export const allRoutes = [
    new Route("/", "Accueil", "pages/home.html"),
    new Route("/galerie", "Galerie", "/pages/galerie.html"),
-   new Route("/carte", "Galerie", "/pages/carte.html"),
-   new Route("/reservations", "Galerie", "/pages/reservations.html"),
-   new Route("/account", "Galerie", "/pages/account.html"),
-   new Route("/login", "Galerie", "/pages/login.html"),
+   new Route("/carte", "Carte", "/pages/carte.html"),
+   new Route("/reservations", "Mes réservations", "/pages/reservations.html"),
+   new Route("/account", "Mon compte", "/pages/auth/account.html"),
+   new Route("/login", "Connexion", "/pages/auth/login.html"),
+   new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+   new Route("/editpassword", "Changement de mot de passe", "/pages/auth/editpassword.html"),
+
 ];
    //Le titre s'affiche comme ceci : Route.titre - websitename
    export const websiteName = "Ô Gâteau";
